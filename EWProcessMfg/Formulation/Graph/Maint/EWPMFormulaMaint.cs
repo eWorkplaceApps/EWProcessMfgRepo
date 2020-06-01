98d90be2bf88501703f961a52a94f7f7ebe17fc1
asharda <asharda@EWAPPS-DEV15>
2020-06-01 02:49:06 -0700

    chnages

---
+++ EWProcessMfg/Formulation/Graph/Maint/EWPMFormulaMaint.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PX.SM;
+using PX.Data;
+
+
+namespace EW.PM.Formulation.Graph.Maint {
+  public class EWPMFormulaMaint:PXGraph<EWPMFormulaMaint> {
+  }
+}
