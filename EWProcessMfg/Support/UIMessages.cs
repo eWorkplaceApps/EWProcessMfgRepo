9df16527ea29db443d51c91fb4c7c70835cf978f
Sanjeev Khanna <skhanna@eworkplaceapps.com>
2020-06-14 21:28:35 -0700

    Add Formula Class and Item Property

--- EWProcessMfg/Support/UIMessages.cs
+++ EWProcessMfg/Support/UIMessages.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EW.PM {
-  public class UIMessages {
+namespace EW.PM
+{
+  public class UIMessages
+  {
+    public const string TestFormulaClassDescFieldName = "Description";
+    public const string TestFormulaClassCD = "ID";
   }
 }
